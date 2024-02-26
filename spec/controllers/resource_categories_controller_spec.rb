6fddfba5fe8afb3f58486c83da14af7840323732
Shane Ludwig <Shane.ludwig01@gmail.com>
2024-02-26 14:43:06 -0800

    Testing whatever changes. Will be overriden

--- spec/controllers/resource_categories_controller_spec.rb
+++ spec/controllers/resource_categories_controller_spec.rb
@@ -2,4 +2,35 @@ require 'rails_helper'
 
 RSpec.describe ResourceCategoriesController, type: :controller do
 
+    # describe "GET #index while logged in" do
+    #     let(:user) { create(:user) }
+
+    #     before(:each) { sign_in(user) }
+
+    #     it { expect(get(:index)).to be_successful }
+    # end
+
+    # describe "GET #index while logged in as admin" do
+    #     let(:admin_user) { create(:user, :admin) }
+
+    #     before(:each) { sign_in(admin_user) }
+
+    #     it { expect(get(:index)).to be_successful }
+    # end
+
+    # describe "GET #index while logged out" do
+    #     it { expect(get(:index)).to redirect_to(new_user_session_path) }
+    # end
+
+    # describe 'GET #new' do
+    #     it { expect(get(:new)).to be_successful }
+    # end
+
+    # describe 'PUT #update' do
+    #     it { expect(get(:update)).to redirect_to(resource_category)}
+    #     it { expect(get(:update, :params[:id = "FAKE"] )).to be_successful}
+    # end
+
+
+
 end
